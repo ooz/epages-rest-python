3f08ee01934568c312e561e5082e13aafca481bf
ooz <ooz@users.noreply.github.com>
2017-09-11 16:00:31 +0200

    Safer env dictionary access

--- tests/context.py
+++ tests/context.py
@@ -6,17 +6,19 @@ author: Oliver Zscheyge <oliverzscheyge@gmail.com>
 
 import os
 import sys
+
 from pytest import fixture
 
 sys.path.insert(0, os.path.abspath('..'))
 import epages
 
 
-EPAGES_BASE_API_URL = os.environ['EPAGES_BASE_API_URL']
-EPAGES_BASE_TOKEN = os.environ['EPAGES_BASE_TOKEN']
+EPAGES_BASE_API_URL = os.environ.get('EPAGES_BASE_API_URL', '')
+EPAGES_BASE_TOKEN = os.environ.get('EPAGES_BASE_TOKEN', '')
 
-EPAGES_NOW_API_URL = os.environ['EPAGES_NOW_API_URL']
-EPAGES_NOW_TOKEN = os.environ['EPAGES_NOW_TOKEN']
+EPAGES_NOW_API_URL = os.environ.get('EPAGES_NOW_API_URL', '')
+EPAGES_NOW_TOKEN = os.environ.get('EPAGES_NOW_TOKEN', '')
+
 
 @fixture
 def given_epages_base_shop():
