9bcd1bc86ee1bcddeda5688eda149b24f0b83181
Tatu Wikman <tatu.wikman@gmail.com>
2018-01-05 15:26:12 +0200

    python3: except syntax in test

--- tests/test_error.py
+++ tests/test_error.py
@@ -8,15 +8,17 @@ from tests.context import epages, given_rest_client, given_beyond_rest_client
 
 NOT_FOUND = 404
 
+
 def test_json_error_for_beyond_shop():
     with given_beyond_rest_client() as client:
         if client is not None:
             try:
                 # when
                 shop = client.get('/shop/willfail')
-            except epages.RESTError, error:
+            except epages.RESTError as error:
                 # then
                 assert error.json['status'] == NOT_FOUND
+
 
 def test_text_error_for_frontend_request():
     with given_beyond_rest_client() as client:
@@ -24,7 +26,7 @@ def test_text_error_for_frontend_request():
             try:
                 # when
                 shop = client.get('/../failzorproduct/and/category')
-            except epages.RESTError, error:
+            except epages.RESTError as error:
                 # then
                 assert error.status_code == NOT_FOUND
                 assert error.method == 'GET'
