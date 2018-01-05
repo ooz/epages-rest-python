b63562add9d95ac06e34f565a39edc1419bda20a
Tatu Wikman <tatu.wikman@gmail.com>
2018-01-05 15:48:40 +0200

    should not require unicode?

--- tests/test_client.py
+++ tests/test_client.py
@@ -9,6 +9,7 @@ from tests.context import \
     is_epages_byd_shop_present, \
     given_rest_client, given_beyond_rest_client
 
+
 def test_client_returning_same_for_relative_and_absolute_queries():
     with given_rest_client() as client:
         if client is not None:
@@ -17,7 +18,8 @@ def test_client_returning_same_for_relative_and_absolute_queries():
             shop_absolute = client.get(EPAGES_BASE_API_URL)
 
             # then
-            assert unicode(shop_relative) == unicode(shop_absolute)
+            assert shop_relative == shop_absolute
+
 
 def test_detecting_api_url_prefix():
     # given
@@ -30,6 +32,7 @@ def test_detecting_api_url_prefix():
     # then
     assert is_prefix
 
+
 def test_still_detecting_api_url_prefix_despite_epages_now_bug():
     # given
     api_url = 'https://lingering-hill-2037.devshop.epages.com/rs/shops/lingering-hill-2037'
@@ -41,9 +44,9 @@ def test_still_detecting_api_url_prefix_despite_epages_now_bug():
     # then
     assert is_prefix
 
+
 def test_getting_beyond_access_token():
     with given_beyond_rest_client() as client:
         if client is not None:
             assert client.beyond, 'Client should be a beyond client!'
             assert client.token != '', 'Beyond client should have a token!'
-
