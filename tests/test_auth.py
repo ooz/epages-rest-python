5c6443be7779e4616d18cab0bb5bf021aadacd90
Tatu Wikman <tatu.wikman@gmail.com>
2018-01-05 15:51:14 +0200

    handle strings as bytes for hmac

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -3,13 +3,12 @@
 description: Tests signature calculation and verification
 author: Oliver Zscheyge <oliverzscheyge@gmail.com>
 '''
-
 from tests.context import epages
 
-CODE = 'rp6YlWEcSxS81OTmdksLOaqv7TRbpzSV'
-ACCESS_TOKEN_URL = 'https://devshop.epages.com/rs/shops/lingering-hill-2037/token'
-CLIENT_SECRET = 'pM8vwvhYTNMErQsNSJZ0wr91ucOmL5JI'
-SIGNATURE = 'g1L73Mv6GnUE8vfWHmxKQmG9KuLiwwI9LMR50w+SXTo='
+CODE = b'rp6YlWEcSxS81OTmdksLOaqv7TRbpzSV'
+ACCESS_TOKEN_URL = b'https://devshop.epages.com/rs/shops/lingering-hill-2037/token'
+CLIENT_SECRET = b'pM8vwvhYTNMErQsNSJZ0wr91ucOmL5JI'
+SIGNATURE = b'g1L73Mv6GnUE8vfWHmxKQmG9KuLiwwI9LMR50w+SXTo='
 
 def test_calculate_signature():
     assert epages.calculate_signature(CODE, ACCESS_TOKEN_URL, CLIENT_SECRET) == SIGNATURE
